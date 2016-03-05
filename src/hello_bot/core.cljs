aae1ed1315255f3d699303988fc8e3d500a07f3d
Joel Birchler <joel@joelbirchler.com>
2016-03-05 07:16:07 +0000

    Express is expressing itself

--- src/hello_bot/core.cljs
+++ src/hello_bot/core.cljs
@@ -2,8 +2,15 @@
   (:require [cljs.nodejs :as nodejs]))
 
 (nodejs/enable-util-print!)
+(defonce express (nodejs/require "express"))
+(def app (express))
+
+(.get app "/" 
+  (fn [req res]
+    (.send res "Howdy.")))
 
 (defn -main [& args]
-  (println "Hello world!"))
+  (.listen app 3000
+    (fn [] (println "Listening on port 3000!"))))
 
 (set! *main-cli-fn* -main)

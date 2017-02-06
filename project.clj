fd291733b474258e242ed5362ebfc46058cee2da
Joel Birchler <joel@joelbirchler.com>
2017-02-05 22:36:10 -0800

    Update license description in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject hello-bot "0.2.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A small robot experiment"
+  :url "http://www.joelbirchler.com"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [
     [org.clojure/clojure "1.8.0"]
     [org.clojure/core.async "0.2.395"]

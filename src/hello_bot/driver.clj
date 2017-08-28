a92c6f02ec998f7ce2011f416e2fc03045e4e6e5
Joel Birchler <joel@joelbirchler.com>
2017-08-28 05:37:42 +0000

    WIP writing a statemap basically works

--- src/hello_bot/driver.clj
+++ src/hello_bot/driver.clj
@@ -2,26 +2,51 @@
   (:require [gpio.core :as gpio]
             [environ.core :refer [env]]))
 
-(defn- pin [device-key]
-  "Returns the env defined pin value for a given device key (eg. :yellow-led)"
-  (env device-key))
+;; We don't want to expose the internals of gpio to the rest of the app, so 
+;; we use a portmap here (:device-key gpioport) to track all of the gpio port
+;; objects. The driver abstraction allows for interactions using the device
+;; key (eg. :yellow-led) and blissful ignorance of the backing gpio library.
+(def portmap (atom {}))
+
+;; TODO: We could make this deluxe by lazy-handling the opening
+;; If you try to write to a device-key that's not in the portmap, open
+;; it and add the shutdown hook
+
 
 (defn open! [device-key]
-  "Opens (and returns) a port for a given device key"
-  (gpio/open-port
-    (pin device-key)
-    :direction :out
-    :digital-result-format :keyword))
-
-(defn turn-on! [device-key]
-  (gpio/write-value! (pin device-key) :high))
-
-(defn turn-off! [device-key]
-  (gpio/write-value! (pin device-key) :low))
+  "Opens a port for a given device key"
+  (swap! portmap assoc device-key (open-output-port! device-key))
+  device-key)
 
 (defn close! [device-key]
   (turn-off! device-key)
-  (gpio/close! (pin device-key)))
+  (gpio/close! (device-key @portmap))
+  device-key)
+
+(defn open-all! [statemap]
+  (do-keys #(open! %) statemap))
+
+(defn close-all! [statemap]
+  (do-keys #(close! %) statemap))
+
+(defn write-state! [statemap]
+  (do-kv write! statemap))
+
+(defn- open-output-port! [device-key]
+  (let [pin (env device-key)]
+    (gpio/unexport! pin)
+    (gpio/open-port pin :direction :out :digital-result-format :keyword)))
+
+(defn- write! [device-key state]
+  (println device-key "->" state)
+  (gpio/write-value! (device-key @portmap) state)
+  device-key)
+
+(defn- turn-on! [device-key]
+  (write! device-key :high))
+
+(defn- turn-off! [device-key]
+  (write! device-key :low))
 
 (defn- do-kv [func statemap]
   (doseq [[k v] statemap] (func k v)))
@@ -29,20 +54,4 @@
 (defn- do-keys [func statemap]
   (doseq [[k _v] statemap] (func k)))
 
-(defn- statemap->pin-states [statemap]
-  (map (fn [[k v]] [(pin k) v]) statemap))
 
-(defn- statemap->pinmap [statemap]
-  (into {} (statemap->pin-states statemap)))
-
-(defn watwat [pin val]
-  (println pin "-" val)) ; gpio/write-value!
-
-(defn set-state! [statemap]
-  (do-kv watwat (statemap->pinmap statemap) ))
-
-(defn open-all! [statemap]
-  (do-keys #(open! %) statemap))
-
-(defn close-all! [statemap]
-  (do-keys #(close! %) statemap))

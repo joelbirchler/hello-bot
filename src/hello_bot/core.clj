d6b92a4351547bf72eab40997163c2b43ffbb18a
Joel Birchler <joel@joelbirchler.com>
2017-12-11 03:11:04 +0000

    First hack at a working bumper

--- src/hello_bot/core.clj
+++ src/hello_bot/core.clj
@@ -5,7 +5,7 @@
   (:gen-class))
 
 (def state (atom car/stop))
-(def bumper-chan (chan))
+(def bumper-chan (driver/read-channel :bumper))
 
 (defn on-state-change [_watch-key _ref _old-state new-state]
   (driver/write! new-state))
@@ -22,19 +22,16 @@
     (mut! new-state)
     (Thread/sleep (* seconds 1000))))
 
-(defn bump! []
-  (>!! bumper-chan :bump))
-
 (defn bumped! []
-  (mut! car/backward 0.1)
-  (mut! car/right 0.3)
+  (println "Bump!")
+  (mut! car/backward 0.2)
+  (mut! car/right 0.2)
   (mut! car/forward))
 
 (defn drive! []
   (mut! car/forward)
   (go-loop []
-    (<! bumper-chan)
-    (bumped!)
+    (if (<! bumper-chan) (bumped!))
     (recur)))
 
 (defn -main [& args]
--- src/hello_bot/driver.clj
+++ src/hello_bot/driver.clj
@@ -1,6 +1,7 @@
 (ns hello-bot.driver
   (:require [gpio.core :as gpio]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]]
+            [clojure.core.async :as async :refer [>! <! >!! <!! go go-loop chan]]))
 
 ;; We don't want to expose the internals of gpio to the rest of the app, so 
 ;; we use a portmap here (:device-key gpioport) to track all of the gpio port
@@ -8,22 +9,31 @@
 ;; key (eg. :yellow-led) and blissful ignorance of the backing gpio library.
 (def open-devices (atom #{}))
 
-(defn- pin [device-key]
+(defn pin [device-key]
   "Returns the pin (number) for a given device-key (eg. :yellow-led)"
   (Integer/parseInt (env device-key)))
 
-(defn- open-output-port! [device-key]
+(defn open-output-port! [device-key]
   "Opens a gpio pin for output"
   (-> (pin device-key)
     (gpio/open-pin)
     (gpio/set-direction :out)))
 
-(defn- init-device! [device-key]
+(defn open-input-port! [device-key]
+  "Opens a gpio pin for input"
+  (-> (pin device-key)
+    (gpio/open-pin)
+    (gpio/set-direction :in)
+    (gpio/active-low true)))
+
+(defn- init-device! [device-key input-or-output]
   "Opens the gpio pin and sets a hook for closing it on shutdown"
-  (open-output-port! device-key)
+  (if (= :input input-or-output)
+    (open-input-port! device-key) 
+    (open-output-port! device-key))
   (swap! open-devices conj device-key)
-  (.addShutdownHook (Runtime/getRuntime) (Thread. 
-                                          #(gpio/close-pin (pin device-key)))))
+  (.addShutdownHook 
+    (Runtime/getRuntime) (Thread. #(gpio/close-pin (pin device-key)))))
 
 (defn- gpio-value [state]
   "Translates :high/:low states to the internal gpio value"
@@ -33,10 +43,20 @@
   "Writes :high/:low to a single device-key (eg. :yellow-led)"
   (println device-key "->" state)
   (when-not (contains? @open-devices device-key)
-    (init-device! device-key))
+    (init-device! device-key :output))
   (gpio/write-value (pin device-key) (gpio-value state)))
 
 (defn write! [statemap]
   "Write to gpio a map of device-keys (eg. :yellow-led) and states (eg. :high, :low)"
   (doseq [[device-key state] statemap]
     (write-to-device-key! device-key state)))
+
+(defn read-channel [device-key]
+  "Returns a channel receives messages from an input port"
+  (when-not (contains? @open-devices device-key) ;; FIXME: I don't like this as a side-effect. Seems unexpected.
+    (init-device! device-key :input))
+  (let [input-chan (chan)]
+    (go-loop []
+      (>! input-chan (gpio/read-value (gpio/wait-for-input (pin device-key))))
+      (recur))
+    input-chan))
